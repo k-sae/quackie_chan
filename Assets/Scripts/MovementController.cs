46d10ad8fad4c46a3b33e474b3fbc5ec01143ae0
Belal <billy.fcih@yahoo.com>
2018-04-06 16:57:57 +0200

    Fix quackie movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class MovementController : MonoBehaviour {
 	public bool isGrounded;
 	private float speed;
-	public float rotSpeed;
+	private float rotSpeed;
 	public float jumpHeight;
 	public bool isSleeping;
 	//walk speed
-	private float w_speed = 0.05f;
+	private float w_speed = 0.1f;
+	//run speed
+	private float r_speed = 0.25f;
 	//rotation speed
-	private float rot_speed = 1.0f;
+	private float rot_speed = 3.0f;
 	Rigidbody rb;
 	Animator anim;
 
@@ -20,84 +22,84 @@ public class MovementController : MonoBehaviour {
 		rb = GetComponent<Rigidbody>();
 		anim = GetComponent<Animator>();
 		isGrounded = true; //indicate that we are in the ground
-		isSleeping= true;
-		}
+		isSleeping = true;
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		//&& anim.GetBool("isJumping") == true
 		if (isGrounded) {
-				//moving forward and backward
-				if (Input.GetKey (KeyCode.W)) {
-					movementControl ("Walking");
-					speed = w_speed;}
-			 
-			//moving right and left
-			else if (Input.GetKey (KeyCode.S)) {
-					movementControl ("WalkingBackward");
-				     rotSpeed = rot_speed;
-				} else {
-					movementControl ("idle");
-				}
-				if (Input.GetKey (KeyCode.A)) {
-					rotSpeed = rot_speed;
-				} else if (Input.GetKey (KeyCode.D)) {
-					rotSpeed = rot_speed;
-				} else {
-					rotSpeed = 0;
-				}
+			//moving forward and backward
+			if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift)) {
+				speed = w_speed;
+				movementControl("WalkingForward");
+			} else if (Input.GetKey(KeyCode.S)) {
+				speed = w_speed;
+				movementControl("WalkingBackward");
+			} else {
+				movementControl("idle");
 			}
-		if (Input.GetKeyDown (KeyCode.Z)) {
-			anim.SetTrigger ("isRuning");
-			rotSpeed = rot_speed;
 
-		}
-			var z = Input.GetAxis ("Vertical") * speed;
-			var y = Input.GetAxis ("Horizontal") * rotSpeed;
-			transform.Translate (0, 0, z);
-			transform.Rotate (0, y, 0);
-			//jumping function
-			if (Input.GetKeyDown (KeyCode.Space) && isGrounded == true) {
-			anim.SetTrigger ("isJumping");
-			rb.AddForce (0, jumpHeight * Time.deltaTime, 0);
-			isGrounded= false;
+			if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W)) {
+				speed = r_speed;
+				movementControl("RunningForward");
 			}
-			
+
+			//moving right and left
+			if (Input.GetKey(KeyCode.A)) {
+				rotSpeed = rot_speed;
+			} else if (Input.GetKey(KeyCode.D)) {
+				rotSpeed = rot_speed;
+			} else {
+				rotSpeed = 0;
+			}
+		}
+		var z = Input.GetAxis("Vertical") * speed;
+		var y = Input.GetAxis("Horizontal") * rotSpeed;
+
+		transform.Translate(0, 0, z);
+		transform.Rotate(0, y, 0);
+
+		// Jumping function
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true) {
+			// TODO: FIX THIS
+			// isGrounded = false;
+			rb.AddForce(0, jumpHeight * Time.deltaTime, 0);
+			anim.SetTrigger("isJumping");
+		}	
 	}
+
 	void movementControl(string state)
 	{
 		switch (state)
 		{
-		case "WalkingForward":
-			anim.SetBool("isWalking", true);
-			anim.SetBool("isWalkingBackward", false);
-			anim.SetBool("isIdle", false);
-			anim.SetBool("hasSprint", false);
-			break;
-		case "WalkingBackward":
-			anim.SetBool("isWalking", false);
-			anim.SetBool("isWalkingBackward", true);
-			anim.SetBool("isIdle", false);
-			anim.SetBool("hasSprint", false);
-			break;
-		case "idle":
-			anim.SetBool("isWalking", false);
-			anim.SetBool("isWalkingBackward", false);
-			anim.SetBool("isIdle", true);
-			anim.SetBool("hasSprint", false);
-			break;
-		case "sprint":
-			anim.SetBool("isWalking", false);
-			anim.SetBool("isWalkingBackward", false);
-			anim.SetBool("isIdle", false);
-			anim.SetBool("hasSprint", true);
-			break;
-			break;
+			case "RunningForward":
+				anim.SetBool("isRunningForward", true);
+				anim.SetBool("isWalkingForward", false);
+				anim.SetBool("isWalkingBackward", false);
+				anim.SetBool("isIdle", false);
+				break;
+			case "WalkingForward":
+				anim.SetBool("isRunningForward", false);
+				anim.SetBool("isWalkingForward", true);
+				anim.SetBool("isWalkingBackward", false);
+				anim.SetBool("isIdle", false);
+				break;
+			case "WalkingBackward":
+				anim.SetBool("isRunningForward", false);
+				anim.SetBool("isWalkingForward", false);
+				anim.SetBool("isWalkingBackward", true);
+				anim.SetBool("isIdle", false);
+				break;
+			case "idle":
+				anim.SetBool("isRunningForward", false);
+				anim.SetBool("isWalkingForward", false);
+				anim.SetBool("isWalkingBackward", false);
+				anim.SetBool("isIdle", true);
+				break;
 		}
 
 	}
-
 
 	void OnCollisionEnter()
 	{
