617d65e34957d2205846322aa6d5c26b462b9a7a
khaledhesham2017 <khaledhesham2017@hotmail.com>
2018-04-09 16:42:25 +0200

    finsh letter clue

--- Assets/Scripts/ClueManger.cs
+++ Assets/Scripts/ClueManger.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ClueManger : MonoBehaviour {
 	public Text subtitles;
 	public static int timeWatch;
+	private Vector3 DBPPL;//diffrent Between postion of  player  and  letter  posion
 	// Use this for initialization
 	void Start () {
 		timeWatch = 0;
@@ -15,21 +16,23 @@ public class ClueManger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (timeWatch < 600) {
+		if (timeWatch < 500) {
 			timeWatch++;
 		}
 
-		if (timeWatch == 300) {
+		if (timeWatch == 250) {
 			showText ("go to duck home to check your children");
-		}
-		if (timeWatch == 600) {
+		} else if (timeWatch == 490) {
 			showText ("");
 		}
-		if (timeWatch <  0) {
-			
-		
-			showText( "your children is kidnapped");
-		}
+		if(timeWatch >490){
+			DBPPL = GameObject.FindGameObjectWithTag("Player").transform.position - GameObject.FindGameObjectWithTag("letter").transform.position;
+			if (  -1.3 < DBPPL [2] &&  DBPPL [2] < 1.3) {
+				showText ("your children is kidnapped , go  to  high place to look  of  them ");
+			} else {
+				showText ("");
+			}
+	}
 	}
 	void showText(string text){
 		subtitles.text = text;
