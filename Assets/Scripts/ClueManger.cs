67def5773b1c6f6e8fb267c3a60215013b32e324
khaledhesham2017 <khaledhesham2017@hotmail.com>
2018-04-09 13:54:50 +0200

    add  subtitels

---
+++ Assets/Scripts/ClueManger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClueManger : MonoBehaviour {
+	public Text subtitles;
+	public static int timeWatch;
+	// Use this for initialization
+	void Start () {
+		timeWatch = 0;
+		showText ("");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeWatch < 600) {
+			timeWatch++;
+		}
+
+		if (timeWatch == 300) {
+			showText ("go to duck home to check your children");
+		}
+		if (timeWatch == 600) {
+			showText ("");
+		}
+		if (timeWatch <  0) {
+			
+		
+			showText( "your children is kidnapped");
+		}
+	}
+	void showText(string text){
+		subtitles.text = text;
+	}
+}
