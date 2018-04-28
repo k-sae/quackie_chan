aa6e99961c4356a893dd9d770509ab37c52af0e6
khaledhesham2017 <khaledhesham2017@hotmail.com>
2018-04-28 20:05:44 +0200

    add text  to hen in land scap

---
+++ Assets/Scripts/HenClue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HenClue : MonoBehaviour {
+
+	public Text subtitles;
+	public Text subtitles2;
+	public static int time;
+	void OnTriggerEnter(Collider other) {
+		subtitles.text = "hen talk";
+		time++;
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if (time == 300) {
+			subtitles.text = "";
+			subtitles2.text = "duck talk";
+		} else {
+			time++;
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		subtitles.text = "";
+		subtitles2.text = "";
+	}
+}
