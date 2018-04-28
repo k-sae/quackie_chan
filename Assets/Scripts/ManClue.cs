1ce5ab4d6d69513f042ac27507c068bf2ec100a5
khaledhesham2017 <khaledhesham2017@hotmail.com>
2018-04-29 00:08:50 +0200

    finsh man  trigger

---
+++ Assets/Scripts/ManClue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ManClue : MonoBehaviour {
+
+	public Text subtitles;
+	public Text subtitles2;
+	public static int time;
+	void OnTriggerEnter(Collider other) {
+		subtitles.text = "man talk";
+		time++;
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if (time == 300) {
+			subtitles.text = "";
+			subtitles2.text = "duck talk";
+		} else {
+			time++;
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{time = 0;
+
+		subtitles.text = "";
+		subtitles2.text = "";
+	}
+}
