0bea04d3c68faa08ddd1e5e42d4fb6087567fa3e
khaledhesham2017 <khaledhesham2017@hotmail.com>
2018-04-29 05:54:47 +0200

    add letter triger

---
+++ Assets/Scripts/letterTriger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class letterTriger : MonoBehaviour {
+
+	public Text subtitles;
+
+	void OnTriggerEnter(Collider other) {
+		
+
+	}
+	void OnTriggerStay(Collider other)
+	{
+		subtitles.text = "man talk";
+	}
+	void OnTriggerExit(Collider other)
+	{
+
+		subtitles.text = "";
+
+	}
+
+}
